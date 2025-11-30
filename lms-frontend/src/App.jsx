3d8015cb0b27a295829c2cd9bf430bd0c8cf8c85
IsamirMalik <153701541+IsamirMalik@users.noreply.github.com>
2025-11-30 18:32:10 +0530

    added basic layout and integrated to homepage

--- lms-frontend/src/App.jsx
+++ lms-frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
 
 function App() {
   return (
     <>
-      {/* <Routes> */}
-
-      {/* </Routes> */}
+      <Routes> 
+        <Route path="/" element={<HomePage />}></Route>
+      </Routes>
     </>
   );
 }
