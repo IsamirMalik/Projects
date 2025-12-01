8e7422cbdc2b913d8b204e489d9041980d84bab8
IsamirMalik <153701541+IsamirMalik@users.noreply.github.com>
2025-12-01 15:12:56 +0530

    added 404 not found page

---
+++ lms-frontend/src/Pages/NotFound.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+function NotFound() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="h-screen flex flex-col items-center justify-center  bg-[#1A2238]">
+      <h1 className="text-9xl font-extrabold text-white tracking-widest">
+        404
+      </h1>
+
+      <div className="bg-black text-white px-2 text-sm rounded rotate-12 absolute">
+        Page Not Found
+      </div>
+      <button className="mt-5">
+        <a className="relative inline-block text-sm font-medium text-[#FF6A3D] group active:text-yellow-500 focus:outline-none focus:ring">
+          <span
+            className="relative block px-8 py-3 bg-[#1A2238] border border-current"
+            onClick={() => navigate(-1)}
+          >
+            Go back
+          </span>
+        </a>
+      </button>
+    </div>
+  );
+}
+
+export default NotFound;
