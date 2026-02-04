900c37588a70ae9d37050cca89e51e52f4356666
IsamirMalik <153701541+IsamirMalik@users.noreply.github.com>
2026-02-04 23:54:20 +0530

    added Product Schema in models directory and also added sample product data in the end for ease of adding product through postman

---
+++ product-ranking/models/product.model.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+
+const productSchema = new Schema({
+  ProductId: Number,
+  model: { type: String, required: true },
+  storage_capacity: { type: String, required: true },
+  release_date: { type: String, required: true },
+  Price: { type: Number, required: true },
+  ratings: { type: Number, default: 0 },
+  reviews: { type: Number, default: 0 },
+  returns: { type: Number, default: 0 },
+  metadata: {
+    ram: { type: String, required: true, enums: ["4GB", "8GB", "14GB"] },
+    storage: { type: String, required: true, enums: ["32GB", "64GB", "128GB", "256GB"] },
+    screensize: { type: String, required: true },
+    brightness: { type: String, required: true },
+  }
+});
+
+const ProductModel = model('product', productSchema);
+
+module.exports = ProductModel;
+
+/**
+ {
+  "model": "Motorola Moto G72",
+  "storage_capacity": "128GB",
+  "release_date": "2023-01-01",
+  "price": 20599,
+  "metadata":{
+    "ram":"6GB",
+    "storage":"128GB" ,
+    "screensize":"6.6-inches",
+  "brightness":"1300nits",
+}
+};
+ 
+*/
