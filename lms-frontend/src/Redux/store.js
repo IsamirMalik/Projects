ba3ac7fd626102233fc3b8a49d9b5d752c2e6fdd
IsamirMalik <153701541+IsamirMalik@users.noreply.github.com>
2026-02-24 14:24:26 +0530

    updated redux store

--- lms-frontend/src/Redux/store.js
+++ lms-frontend/src/Redux/store.js
@@ -1,10 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authSliceReducer from "./Slices/AuthSlice.js";
+
+import authReducer from './slices/authSlice.js';
+import courseReducer from './slices/courseSlice.js';
+import lectureReducer from './slices/lectureSlice.js';
+import razorpayReducer from './slices/razorPaySlice.js';
+import statSliceReducer from './slices/StatSlice.js';
+
 
 const store = configureStore({
   reducer: {
-    auth : authSliceReducer
+    auth: authReducer,
+    course: courseReducer,
+    razorpay: razorpayReducer,
+    lecture: lectureReducer,
+    stat : statSliceReducer
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
   devTools: true
 });
 
