a070f68f47679b77361a3de563dab522fcea34c3
IsamirMalik <153701541+IsamirMalik@users.noreply.github.com>
2026-02-24 14:27:55 +0530

    updated login page

--- lms-frontend/src/Pages/Login.jsx
+++ lms-frontend/src/Pages/Login.jsx
@@ -1,105 +1,75 @@
-import React from "react";
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import HomeLayout from "../Components/HomeLayout";
+import toast from "react-hot-toast";
 import { useDispatch } from "react-redux";
-import { toast } from "react-hot-toast";
-import { login } from "../Redux/Slices/AuthSlice";
+import { Link, useNavigate } from "react-router-dom";
+
+import { isEmail } from "../helpers/regexMatcher";
+import HomeLayout from "../layouts/HomeLayout";
+import { login } from "../redux/slices/authSlice";
 
 function Login() {
-  const dispatch = useDispatch();
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-  const [loginData, setLoginData] = useState({
+  const [signinDetails, setSigninDetails] = useState({
     email: "",
     password: "",
   });
 
-  function handelUserInput(e) {
+  function handleUserInput(e) {
     const { name, value } = e.target;
-    console.log(name, value);
-
-    setLoginData({
-      ...loginData,
+    setSigninDetails({
+      ...signinDetails,
       [name]: value,
     });
-  }  
+  }
 
-  async function onLogin(event) {
-    event.preventDefault();
-
-    if (!loginData.email || !loginData.password) {
-      toast.error("All fields are required");
+  async function onFormSubmit(e) {
+    e.preventDefault();
+    console.log(signinDetails);
+    if (!signinDetails.email || !signinDetails.password) {
+      toast.error("Please fill all the details");
       return;
-    }         
-
-    if(loginData.email && loginData.password){
-        const email = loginData.email;
-        const password = loginData.password;
-
-        const userEmail = JSON.parse(localStorage.getItem("email"));
-        const userPassword = JSON.parse(localStorage.getItem("password"));
-
-        if(email == userEmail && password == userPassword){
-          console.log("Logged in successfully");
-            navigate("/");
-        }
+    }
+    if (!isEmail(signinDetails.email)) {
+      toast.error("Invalid email provided");
+      return;
     }
 
-    // dispatch create account action
-    // const response = await dispatch(login(loginData));
-    // console.log(response)
-    // if (response?.payload?.success) {
-    //   navigate("/");
-    // }
-
-    // setLoginData({
-    //   email: "",
-    //   password: "",
-    // });
-
-
-    toast.success("Logged in successfully");
+    const response = await dispatch(login(signinDetails));
+    console.log(response);
+    if (response?.payload?.data) {
+      navigate("/");
+    }
+    setSigninDetails({
+      email: "",
+      password: "",
+    });
   }
 
   return (
     <HomeLayout>
-      <div className="flex items-center justify-center h-[90vh]">
+      <div className="flex overflow-x-auto items-center justify-center h-[100vh]">
         <form
-          onSubmit={onLogin}
+          onSubmit={onFormSubmit}
           noValidate
           className="flex flex-col justify-center gap-3 rounded-lg p-4 text-white w-96 shadow-[0_0_10px_black]"
         >
-          <h1 className="text-center text-2xl font-bold">Login Page</h1>
-          
-          {/* <div className="flex flex-col gap-1">
-            <label htmlFor="full_name" className="font-semibold">
-              Full Name
-            </label>
-            <input
-              type="text"
-              required
-              id="full_name"
-              name="full_name"
-              placeholder="Enter your full name here"
-              className="bg-transparent px-2 py-1 border rounded-sm"
-              onChange={handelUserInput}
-              value={loginData.full_name}
-            />
-          </div> */}
+          <h1 className="text-2xl text-center font-bold">Login Page</h1>
+
           <div className="flex flex-col gap-1">
             <label htmlFor="email" className="font-semibold">
               Email
             </label>
             <input
-              type="email"
+              onChange={handleUserInput}
+              value={signinDetails.email}
               required
-              id="email"
+              type="text"
               name="email"
-              placeholder="Enter your email here"
               className="bg-transparent px-2 py-1 border rounded-sm"
-              onChange={handelUserInput}
-              value={loginData.email}
+              placeholder="enter your Email..."
+              id="email"
             />
           </div>
           <div className="flex flex-col gap-1">
@@ -107,25 +77,22 @@ function Login() {
               Password
             </label>
             <input
-              type="password"
               required
-              id="password"
+              onChange={handleUserInput}
+              value={signinDetails.password}
+              type="password"
               name="password"
-              placeholder="Enter your password here"
               className="bg-transparent px-2 py-1 border rounded-sm"
-              onChange={handelUserInput}
-              value={loginData.password}
+              placeholder="enter your Password..."
+              id="password"
             />
           </div>
-          <button
-            className=" font-semibold bg-yellow-700 hover:bg-yellow-500 transition-all ease-in-out duration-300 py-1 text-black text-lg rounded-sm cursor-pointer"
-            type="submit"
-          >
-            Login
+          <button className="mt-2 bg-yellow-800 hover:bg-yellow-500 transition-all ease-in-out duration-300 cursor-pointer py-2 font-semibold text-lg rounded-sm">
+            Sign In
           </button>
           <p className="text-center">
-            Do not have an account ?{" "}
-            <Link to="/signup" className="text-yellow-600 cursor-pointer">
+            Donot have an account ?{" "}
+            <Link to="/signup" className="cusror-pointer text-accent">
               SignUp
             </Link>
           </p>
