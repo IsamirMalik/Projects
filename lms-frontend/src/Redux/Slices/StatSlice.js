b4d9d4c90104cbca0752d8461bb2f57c8403f82e
IsamirMalik <153701541+IsamirMalik@users.noreply.github.com>
2026-02-24 14:35:00 +0530

    added stats slice

---
+++ lms-frontend/src/Redux/Slices/StatSlice.js
@@ -0,0 +1,41 @@
+import { createAsyncThunk , createSlice } from "@reduxjs/toolkit";
+import toast from "react-hot-toast";
+
+import axiosInstance from "../../Helpers/axiosInstance.js";
+
+const initialState = {
+  allUsersCount : 0,
+  subscribedCount:0
+}
+
+export const getStatsData = createAsyncThunk('stats/get', async () => {
+  try {
+    const response = axiosInstance.get("/admin/stats/users");
+    toast.promise(response, {
+      loading: "Fetching stats",
+      success: (data) => {
+        return data?.data?.message;
+      },
+      error: "Failed to fetch stats"
+    });
+    console.log((await response).data.data);
+    return (await response).data.data;
+  } catch (error) {
+    toast.error(error?.response?.data?.message);
+  }
+});
+
+const statSlice = createSlice({
+  name: "stat",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getStatsData.fulfilled, (state, action) => {
+      state.allUsersCount = action.payload.totalUsers;
+      state.subscribedCount = action.payload.totalActiveSubscriptions;
+      state.totalRevenue = action.payload.totalRevenue;
+    });
+  },
+});
+
+export default statSlice.reducer;
